a48696a4b18730ebc5b890bf8b7524307a30e275
Cerveira <ivaldo13@outlook.com>
2020-09-03 13:28:59 -0300

    Fixes

--- examples/ch10/Fig10_01/PolymorphismTest.java
+++ examples/ch10/Fig10_01/PolymorphismTest.java
@@ -25,26 +25,8 @@ public class PolymorphismTest {
          basePlusCommissionEmployee.toString());
 
       // invoke toString on subclass object using superclass variable
-      CommissionEmployee commissionEmployee2 =
-         basePlusCommissionEmployee;          
-      System.out.printf("%s %s:%n%n%s%n", 
+      System.out.printf("%s %s:%n%n%s%n",
          "Call BasePlusCommissionEmployee's toString with superclass",
-         "reference to subclass object", commissionEmployee2.toString());
+         "reference to subclass object", ((CommissionEmployee) basePlusCommissionEmployee).toString());
    } 
 } 
-
-
-/**************************************************************************
- * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
