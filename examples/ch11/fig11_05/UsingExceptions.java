5fab9e74ffa87450bd6f1b70a93cb71a01031d87
Cerveira <ivaldo13@outlook.com>
2020-09-11 18:17:39 -0300

    Initial commit

--- examples/ch11/fig11_05/UsingExceptions.java
+++ examples/ch11/fig11_05/UsingExceptions.java
@@ -40,6 +40,7 @@ public class UsingExceptions {
          System.out.println("Method doesNotThrowException");
       } 
       catch (Exception exception) { // does not execute
+         //noinspection ThrowablePrintedToSystemOut
          System.err.println(exception);
       }
       finally { // executes regardless of what occurs in try...catch     
@@ -49,18 +50,3 @@ public class UsingExceptions {
       System.out.println("End of method doesNotThrowException");
    } 
 } 
-
-/**************************************************************************
- * (C) Copyright 1992-2018 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
